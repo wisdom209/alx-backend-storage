c179771e0e17ec2ffb837e733a07c488690b3aa7
Wisdom <ononiwuwisdom2@gmail.com>
2023-07-18 05:38:25 -0700

    find doc list

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,6 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """get schools by topic"""
-    docs =  mongo_collection.find({"topic": topic})
-    doclist = [doc for doc in docs]
-    return doclist
+    return  mongo_collection.find({"topic": {"$in": [topic]}})
