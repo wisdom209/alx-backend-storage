d227fe63d45bf05acdf7dc072a08d7ae9cc5cbdf
Wisdom <ononiwuwisdom2@gmail.com>
2023-07-18 05:00:43 -0700

    list collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Pymongo script"""
+from pymongo import MongoClient
+client = MongoClient()
+db = client.my_db
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    return mongo_collection.find()
