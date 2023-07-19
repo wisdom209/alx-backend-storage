01f1b1df414e0cf71dfd181fea416e34914f4bea
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-19 20:53:49 +0100

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,11 +2,12 @@
 """writing strings to redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
     """cache class"""
+
     def __init__(self):
         """initial function"""
         self._redis = redis.Redis()
@@ -17,3 +18,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """read from redis"""
+        if key is None:
+            return None
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """parametrize cache.get"""
+        value = self.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """parametrize cache.get"""
+        value = self.get(key)
+        try:
+            return int(value.decode('utf-8'))
+        except Exception:
+            return 0
